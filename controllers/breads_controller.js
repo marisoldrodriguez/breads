16f5df6dbef9e67c343ffaf0e9da3e8d80f78f5f
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-07 14:20:31 -0400

    first commit

---
+++ controllers/breads_controller.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const breads = express.Router()
+const bread = require('../models/bread')
+
+// INDEX
+breads.get('/', (req, res) => {
+  res.send(bread)
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(bread[req.params.arrayIndex])
+  })
+  
+
+module.exports = breads
+
