4e47cffd0907dd682848c7cd4023e8d6c5fecb93
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-11 19:11:49 -0400

    Navbar with style

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 // DEPENDENCIES
-const express = require('express')
-const methodOverride = require('method-override')
+const express = require("express");
+const methodOverride = require("method-override");
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
 
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads");
+});
+
 // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
- 
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
+
 // 404 Page
-app.get('*', (req, res) => {
-  res.render('error404')
-})
-  
+app.get("*", (req, res) => {
+  res.render("error404");
+});
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
-})
+  console.log("nomming at port", PORT);
+});
