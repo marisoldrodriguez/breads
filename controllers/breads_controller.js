a9a78d24a4904e8fc4518b4d8e2310712cc2231a
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-20 22:41:52 -0400

    implemented seed route (.insertMany)

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,65 +1,125 @@
 const express = require("express");
+const Bread = require("../models/bread");
 const breads = express.Router();
-const bread = require("../models/bread");
 
 // INDEX
-breads.get("/", (req, res) => {
-  res.render("Index", {
-    breads: bread,
-    title: "Index Page",
-  });
-  // res.send(Bread)
-});
+breads.get('/', (req, res) => {
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
+})
+
+// INSERT MANY (SEEDS)
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany([
+    {
+      name: 'Rye',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'French',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'Gluten Free',
+      hasGluten: false,
+      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'Pumpernickel',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    }
+  ]
+  )
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+    .catch(err => {
+      res.render('error404')
+    })
+})
 
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
-    req.body.hasGluten === 'true'
+    req.body.hasGluten = true
   } else {
-    req.body.hasGluten === 'false'
+    req.body.hasGluten = false
   }
-  bread.push(req.body)
+  console.log(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
+
+
 
 // NEW
 breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id)
+  .then(foundBread => {
+    res.render('edit', {
+      bread: foundBread
+    })
+
+  })
+  
+})
+
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then(updatedBread => {
+    console.log(updatedBread)
+    res.redirect(`/breads/${req.params.id}`)
+  })
+  
 })
 
 // SHOW
-breads.get("/:arrayIndex", (req, res) => {
-  if (bread[req.params.arrayIndex]) {
-    res.render("Show", {
-      bread: bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    });
-  } else {
-    res.render("404");
-  }
-});
-
-
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
+    })
+    .catch(err => {
+      res.render('error404')
+    })
+})
 
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  bread.splice(req.params.indexArray, 1)
+breads.delete('/:id', (req, res) => {
+ Bread.findByIdAndDelete(req.params.id)
+ .then(deletedBread => {
   res.status(303).redirect('/breads')
+  console.log(deletedBread)
+ })
+ 
 })
+
+
+
 
 module.exports = breads;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const express = require("express");
 const methodOverride = require("method-override");
+const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require("dotenv").config();
@@ -14,6 +16,12 @@ app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride("_method"));
+
+// MONGOOSE
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 
 // ROUTES
